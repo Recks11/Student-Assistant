4ec3e4621c183941fc3a7b630d51c100996a08ca
Ijiekhuamen Rex <rexijie@gmail.com>
2018-03-17 02:28:17 +0000

    Fixed user registration page to add user to login database

--- src/main/java/com/emmanuelirem/studentassistant/controllers/RegisterController.java
+++ src/main/java/com/emmanuelirem/studentassistant/controllers/RegisterController.java
@@ -1,20 +1,33 @@
 package com.emmanuelirem.studentassistant.controllers;
 
+import com.emmanuelirem.studentassistant.Models.Security.Roles;
+import com.emmanuelirem.studentassistant.Models.Security.Users;
 import com.emmanuelirem.studentassistant.Models.Student;
+import com.emmanuelirem.studentassistant.Repository.RolesService;
 import com.emmanuelirem.studentassistant.Repository.StudentRepository;
+import com.emmanuelirem.studentassistant.Repository.UsersService;
+import com.emmanuelirem.studentassistant.Services.EncoderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RegisterController {
 
+    private final EncoderService encoderService;
+
     private StudentRepository studentRepository;
 
-    public RegisterController (StudentRepository studentRepository){
+    private final UsersService usersService;
+    private final RolesService rolesService;
+    @Autowired
+    public RegisterController(StudentRepository studentRepository, EncoderService encoderService,
+                              UsersService usersService, RolesService rolesService){
         this.studentRepository = studentRepository;
+        this.encoderService = encoderService;
+        this.usersService = usersService;
+        this.rolesService = rolesService;
     }
 
     @GetMapping("/register")
@@ -23,10 +36,23 @@ public class RegisterController {
         model.addAttribute("student", new Student());
         return "register";
     }
+
     @PostMapping("/register")
     public String registerStudent(@ModelAttribute Student student){
 
         try{
+            Users newUser = new Users();
+            Roles userRole = new Roles();
+            newUser.setRegistrationNumber(student.getRegistrationNumber());
+            newUser.setPassword(encoderService.passwordEncoder().encode(student.getPassword()));
+            newUser.setEnabled(true);
+
+            userRole.setRegistrationNumber(student.getRegistrationNumber());
+            userRole.setRole("ROLE_USER");
+
+            usersService.save(newUser);
+            rolesService.save(userRole);
+
             studentRepository.save(student);
         } catch (Exception e) {
             System.out.println(e);
